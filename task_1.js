7eeaab3f756ff55106fc0bd2c4e396cfb2ac7e05
Roman Korotaev <kro1986@ukr.net>
2021-08-15 23:16:28 +0300

    test

--- task_1.js
+++ task_1.js
@@ -1,1 +1,48 @@
-console.log("task_1")
+console.log("task_1");
+
+
+// Напиши функцию delay(ms), которая возвращает промис, переходящий в состояние "resolved" через ms миллисекунд.
+// Значением исполнившегося промиса должно быть то кол - во миллисекунд которое передали во время вызова функции delay.
+
+// const delay = ms => {
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve(ms);
+//     }, ms);
+//   });
+// };
+
+// const logger = time => console.log(`Resolved after ${time}ms`);
+
+// // Вызовы функции для проверки
+// delay(2000).then(logger); // Resolved after 2000ms
+// delay(1000).then(logger); // Resolved after 1000ms
+// delay(1500).then(logger); // Resolved after 1500ms
+
+
+// //---------------------------
+
+// const promise = new Promise((resolve, reject) => {
+//   const canFulfill = Math.random() > 0.5;
+
+//   setTimeout(() => {
+//     if (canFulfill) {
+//       resolve('Промис выполнился успешно, с результатом (исполнен, fulfilled)');
+//     }
+
+//     reject('Промис выполнился с ошибкой! (отклонён, rejected)');
+//   }, 1000);
+// });
+
+//   //promise.then(onFulfilled, onRejected);
+//    promise.then(onFulfilled,  onRejected);
+
+// function onFulfilled(result) {
+//   console.log('onFulfilled -> onFulfilled');
+//   console.log(`✅ ${result}`);
+// }
+
+// function onRejected(error) {
+//   console.log('onRejected -> onRejected');
+//   console.log(`❌ ${error}`);
+// }
